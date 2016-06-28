ee77e21f502f3e328c962fef0c1e2184bcd3f544
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-28 09:56:47 -0700

    init commit

---
+++ vic-cowsay-service/server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const express = require(express);
+const app = express();
+
+app.use(express.static(__dirname + '/build'));
+app.listen(3001, () => console.log('listening on 3001'));
